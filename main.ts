195a70af85620ecc80ceae570da6328b112ec37a
pomdtr <achille.lacoin@gmail.com>
2024-09-30 04:21:54 +0200

    remove need for embedding

--- main.ts
+++ main.ts
@@ -1,13 +1,5 @@
-import { join } from "jsr:@std/path";
 import { Excalidraw } from "./mod.ts";
 
-const excalidraw = new Excalidraw({
-    read(key) {
-        return Deno.readFile(join("blobs", key));
-    },
-    write(key, value) {
-        return Deno.writeFile(join("blobs", key), value);
-    },
-});
+const excalidraw = new Excalidraw("./blobs");
 
 export default excalidraw;
--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { Hono } from "jsr:@hono/hono@4.4.8";
-import { decodeBase64 } from "jsr:@std/encoding@0.224.0/base64";
-import { serveDir } from "./dist/dir.ts";
+import { decodeBase64 } from "jsr:@std/encoding@1.0.1";
+import manifest from "./deno.json" with { type: "json" };
+import { join } from "jsr:@std/path@1.0.0/join";
 
 const keys = {
     json: "drawing.excalidraw.json",
@@ -14,7 +15,8 @@ export type ExcalidrawParams = {
 };
 
 export class Excalidraw {
-    constructor(private params: ExcalidrawParams) {}
+    constructor(public rootDir: string) {
+    }
     fetch = (req: Request): Response | Promise<Response> => {
         const app = new Hono();
 
@@ -22,11 +24,11 @@ export class Excalidraw {
             const { json, png, svg } = await c.req.json();
 
             const jsonBytes = new TextEncoder().encode(json);
-            await this.params.write(keys.json, jsonBytes);
+            await Deno.writeFile(join(this.rootDir, keys.json), jsonBytes);
             const pngBytes = decodeBase64(png);
-            await this.params.write(keys.png, pngBytes);
+            await Deno.writeFile(join(this.rootDir, keys.png), pngBytes);
             const svgBytes = new TextEncoder().encode(svg);
-            await this.params.write(keys.svg, svgBytes);
+            await Deno.writeFile(join(this.rootDir, keys.svg), svgBytes);
 
             return new Response(null, {
                 status: 204,
@@ -34,31 +36,52 @@ export class Excalidraw {
         });
 
         app.get("/png", async () => {
-            return new Response(await this.params.read(keys.png), {
-                headers: {
-                    "Content-Type": "image/png",
+            return new Response(
+                await Deno.readFile(join(this.rootDir, keys.png)),
+                {
+                    headers: {
+                        "Content-Type": "image/png",
+                    },
                 },
-            });
+            );
         });
 
         app.get("/svg", async () => {
-            return new Response(await this.params.read(keys.svg), {
-                headers: {
-                    "Content-Type": "image/svg+xml",
+            return new Response(
+                await Deno.readFile(join(this.rootDir, keys.svg)),
+                {
+                    headers: {
+                        "Content-Type": "image/svg+xml",
+                    },
                 },
-            });
+            );
         });
 
         app.get("/json", async () => {
-            return new Response(await this.params.read(keys.json), {
-                headers: {
-                    "Content-Type": "application/json",
+            return new Response(
+                await Deno.readFile(join(this.rootDir, keys.json)),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 },
-            });
+            );
         });
 
-        app.get("/*", (c) => {
-            return serveDir(c.req.raw);
+        app.get("*", async (c) => {
+            const filepath = c.req.path === "/" ? "index.html" : c.req.path.slice(1);
+            const url = `https://raw.esm.sh/jsr/${manifest.name}@${manifest.version}/frontend/dist/${filepath}`
+            const resp = await fetch(url);
+            if (!resp.ok) {
+                return new Response(null, {
+                    status: 404,
+                });
+            }
+
+            const html = await resp.text();
+            return new Response(html, {
+                headers: resp.headers
+            });
         });
 
         return app.fetch(req);
