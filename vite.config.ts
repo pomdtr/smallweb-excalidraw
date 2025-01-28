2d86e63b11db90b9d0a6d342276854d72364390e
pomdtr <achille.lacoin@gmail.com>
2025-01-28 18:35:50 +0100

    remove unecessary smallweb config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'deno/pkg/static',
+    outDir: 'dist/pkg/static',
   },
   plugins: [react()],
 })
