f79c9021bcba24b8f4040966b600907ffa92020b
pomdtr <achille.lacoin@gmail.com>
2025-01-28 16:45:53 +0100

    rework file structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../pkg/static',
-    emptyOutDir: true,
+    outDir: 'deno/pkg/static',
   },
   plugins: [react()],
 })
