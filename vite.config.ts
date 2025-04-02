864de990c0bf1ec86224f94c34fa4b8c6d0d4b10
pomdtr <achille.lacoin@gmail.com>
2025-04-02 12:50:51 +0200

    fix dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "pkg/static",
+    outDir: "dist/static",
   },
   plugins: [react()],
 })
